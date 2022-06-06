d6683bf07585320e657ec24d0028edd0bc2143e3
Rebecca Lim <rebeccalim23@gmail.com>
2022-06-06 17:31:44 +0100

    update stages to completions

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :game
-  has_many :stages, dependent: :destroy
+  has_many :completions, dependent: :destroy
   has_many :challenges, through: :stages
 end
