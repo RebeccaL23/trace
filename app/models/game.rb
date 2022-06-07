3dbd2989d6e88ca7bbfbd0e1e42ccf9e93925e0a
Adrian Hards <github@ahards.com>
2022-06-07 21:59:49 +0100

    changed form to correct format

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,12 @@ class Game < ApplicationRecord
   has_many :challenges, dependent: :destroy
   validates :name, :city, :start, :end, presence: true
   validates :id, uniqueness: true
+
+  # search on a user's index page
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+                  against: [:name],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
