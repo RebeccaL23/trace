d4a08818e685488c4b06c0d475528221cdc298b2
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-06-07 12:42:46 +0100

    Added location to challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,5 +2,5 @@ class Challenge < ApplicationRecord
   belongs_to :game
   has_many :completions
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
