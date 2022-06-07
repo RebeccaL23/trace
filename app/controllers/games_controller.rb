3f27b74d53f80dff44aa988fd87cb7563cac2ea9
Adrian Hards <github@ahards.com>
2022-06-07 12:29:51 +0100

    completed games controller and most of challenges

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,58 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: %i[show edit update destroy confirmation]
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  # not checked!
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save
+      redirect_to game_confirmation_path(@game.id), notice: 'you created a game!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # not checked!
+  def update
+    @game.update(game_params)
+    redirect_to game_path(@game)
+  end
+
+  # not checked!
+  def edit
+  end
+
+  # not checked! is it simply current_user or current_user.id
+  def destroy
+    @game.destroy
+    redirect_to user_path(current_user.id), status: :see_other
+  end
+
+  # not checked!
+  def confirmation
+    # @games = Game.all
+    # if @game.id < 6 chars
+    #   while @game.id matches ids in @games
+    #     new = generate 6 digits
+    #     @game.id = new
+    #   end
+    # end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:name, :city, :start, :end, :photo)
+  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,8 @@
 class TeamsController < ApplicationController
 end
+
+  # @completion = Completion.create(completed: false, team: @team, challenge: @challenge)
+
+  # def set_team
+  #   @team = Team.find(params[:id])
+  # end
