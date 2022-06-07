729cc8ad2b8bca09a26839532c3b2faab3641a13
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-06-07 11:56:18 +0100

    Basic seed file with users, teams, games and challenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,118 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Team.destroy_all
+Game.destroy_all
+Challenge.destroy_all
+
+challenges = Challenge.all
+teams = Team.all
+games = Game.all
+
+user1 = User.create(
+  email: "Rebecca@gmail.com",
+  password: "123456"
+)
+
+user2 = User.create(
+  email: "adrian@gmail.com",
+  password: "123456"
+)
+
+user3 = User.create(
+  email: "padre@gmail.com",
+  password: "123456"
+)
+
+puts "Created 3 users!"
+
+Game.create(
+  name: "Jack the Ripper",
+  city: "London",
+  start: "12-06-2022 10:00",
+  end: "12-06-2022 13:00",
+  user: user1
+)
+
+Game.create(
+  name: "Jurassic Park",
+  city: "London",
+  start: "14-06-2022 10:00",
+  end: "14-06-2022 13:00",
+  user: user1
+)
+
+Game.create(
+  name: "Ancient Aliens",
+  city: "London",
+  start: "14-06-2022 10:00",
+  end: "14-06-2022 13:00",
+  user: user1
+)
+
+puts "Created 3 games!"
+
+team1 = Team.new(
+  name: "Firestarters",
+  game: games[0]
+)
+team1.save!
+
+team2 = Team.new(
+  name: "Spice Boyz",
+  game: games[0]
+)
+team2.save!
+
+team1 = Team.new(
+  name: "Firestarters",
+  game: games[1]
+)
+team1.save!
+
+team2 = Team.new(
+  name: "Spice Boyz",
+  game: games[1]
+)
+team2.save!
+team1 = Team.new(
+  name: "Firestarters",
+  game: games[2]
+)
+team1.save!
+
+team2 = Team.new(
+  name: "Spice Boyz",
+  game: games[2]
+)
+team2.save!
+
+puts "Created 2 teams per game!"
+
+16.times do
+  Challenge.create(
+    question: "How many lions are there in trafalgar square?",
+    answer_1: "3",
+    answer_2: "1",
+    answer_3: "8",
+    correct_answer: "4",
+    points: 10,
+    location: "Trafalgar Square",
+    game: games.sample
+  )
+end
+
+puts "Created 16 challenges!"
+
+
+# 16.times do
+#   Completion.create(
+#     completed: false,
+#     team: teams.sample,
+#     challenge: challenges.sample
+#   )
+# end
+
+# puts "Created 16 completions"
