90aecb83d8ca8a2cd00912eb2eb9dc7356fc0c52
Rebecca Lim <rebeccalim23@gmail.com>
2022-06-08 14:12:39 +0100

    add modal for challenge edit

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import ChallengeController from "./challenge_controller.js"
+application.register("challenge", ChallengeController)
+
 import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
 
