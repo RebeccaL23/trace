9ad19614b711cef2744e3df0e72cfa4fa7486ce0
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-06-08 16:07:06 +0100

    added markers to map, created marker component

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,5 +13,28 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #addMarkersToMap() {
+
+    this.markersValue.forEach((marker) => {
+      const customMarker = document.createElement("div")
+      customMarker.style.backgroundSize = "contain"
+      customMarker.classList.add("unfound-marker");
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        // .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3 })
   }
 }
