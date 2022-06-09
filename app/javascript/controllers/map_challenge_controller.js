414eab4fb21c32ee8e249008f9c7dfb0f8eed3ff
Rebecca Lim <rebeccalim23@gmail.com>
2022-06-09 14:59:42 +0100

    update challenge map and challenge form per challenge id

--- app/javascript/controllers/map_challenge_controller.js
+++ app/javascript/controllers/map_challenge_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
+    marker: Array
   }
 
   connect() {
@@ -15,15 +15,15 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
-    document.querySelector('.mapboxgl-canvas').style.width = '42vw';
-    document.querySelector('.mapboxgl-canvas').style.height = '84vh';
-    this.map.resize();
+    // document.querySelector('.mapboxgl-canvas').style.width = '42vw';
+    // document.querySelector('.mapboxgl-canvas').style.height = '84vh';
+    // this.map.resize();
 
     const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.markerValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
 
-    this.markersValue.forEach((marker) => {
+    this.markerValue.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
