e4dbe59441d977e04bb3edc4f38136804f50d23b
Adrian Hards <github@ahards.com>
2022-06-10 15:53:19 +0100

    skip authentication

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ require "chunky_png"
 
 class GamesController < ApplicationController
   before_action :set_game, only: %i[show edit update destroy]
-  skip_before_action :authenticate_user!, only: %i[confirmation]
+  skip_before_action :authenticate_user!, only: %i[confirmation join]
 
   def show
     @challenge = Challenge.new
