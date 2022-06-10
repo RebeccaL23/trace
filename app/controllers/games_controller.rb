97e1c0d27381f4d5641676b7a7a99ad184c9fa8e
Adrian Hards <github@ahards.com>
2022-06-10 15:12:34 +0100

    hard code qr code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -59,22 +59,22 @@ class GamesController < ApplicationController
     end
 
   # qrcode = RQRCode::QRCode.new("#{request.original_url}")
-  qrcode = RQRCode::QRCode.new("https://trace-game.herokuapp.com/join")
+  # qrcode = RQRCode::QRCode.new("https://trace-game.herokuapp.com/join")
 
-  png = qrcode.as_png(
-    bit_depth: 1,
-    border_modules: 1,
-    color_mode: ChunkyPNG::COLOR_GRAYSCALE,
-    color: "black",
-    file: nil,
-    fill: "white",
-    module_px_size: 10,
-    resize_exactly_to: false,
-    resize_gte_to: false,
-    size: 250
-  )
+  # png = qrcode.as_png(
+  #   bit_depth: 1,
+  #   border_modules: 1,
+  #   color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+  #   color: "black",
+  #   file: nil,
+  #   fill: "white",
+  #   module_px_size: 10,
+  #   resize_exactly_to: false,
+  #   resize_gte_to: false,
+  #   size: 250
+  # )
 
-  IO.binwrite("./app/assets/images/qr_code#{@game.code}.png", png.to_s)
+  # IO.binwrite("./app/assets/images/qr_code#{@game.code}.png", png.to_s)
 
   end
 
