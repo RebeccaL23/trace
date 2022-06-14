92a522c59cf3a404a9c8fe7d337da4f2dda50deb
Rebecca Lim <rebeccalim23@gmail.com>
2022-06-14 16:35:12 +0100

    add leaderboard routes and basic view page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :games, only: %i[show new create edit update] do
     get "status", to: "games#status", as: :status
     get "confirmation", to: "games#confirmation", as: :confirmation
-    get "leader", to: "games#leaderboard", as: :leaderboard
+    get "leaderboard", to: "games#leaderboard", as: :leaderboard
     # teams
     resources :teams, only: %i[show new create] do
       get "play", to: "games#play", as: :play
