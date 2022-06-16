ad50000aedd42bd0397eee4a6948d8c1049c1951
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-06-16 18:12:50 +0100

    Fixed bug with destroying challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,6 @@
 class Challenge < ApplicationRecord
   belongs_to :game
-  has_many :completions
+  has_many :completions, dependent: :destroy
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
