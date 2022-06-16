023ac5ede6323c9575b85e44862ae3e084d357c1
Rebecca Lim <rebeccalim23@gmail.com>
2022-06-16 15:50:57 +0100

    update player play styling - tutorial modal and challenge pop up

--- app/javascript/controllers/mobile_play_controller.js
+++ app/javascript/controllers/mobile_play_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="mobile-play"
 export default class extends Controller {
-  static targets = [ "show1", "show2", "show3", "show4", "show5", "show6", "height"]
+  static targets = [ "show1", "show2", "show3", "show4", "show5", "show6","show7", "height"]
 
   gameDetails(){
     this.show1Target.classList.toggle("mobile-play-hide");
@@ -11,6 +11,7 @@ export default class extends Controller {
     this.show4Target.classList.toggle("mobile-play-hide");
     this.show5Target.classList.toggle("mobile-play-hide");
     this.show6Target.classList.toggle("mobile-play-hide");
+    this.show7Target.classList.toggle("mobile-play-hide");
     this.heightTarget.classList.toggle("height");
   }
 }
