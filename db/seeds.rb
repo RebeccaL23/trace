611cbc1f3e1363a5cfeba7e37398b1172e44ac4b
Adrian Hards <github@ahards.com>
2022-06-16 07:24:26 +0100

    first changes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,9 @@ Challenge.destroy_all
 
 puts "Destroyed users, teams, games and challenges"
 
-challenges = Challenge.all
-teams = Team.all
-games = Game.all
+# challenges = Challenge.all
+# teams = Team.all
+# games = Game.all
 
 puts "Creating users..."
 
@@ -29,7 +29,6 @@ puts "Created #{User.count} user!"
 
 puts "Creating 9 games..."
 
-
 Game.create(
   name: "Rebecca's Tots",
   city: "London",
@@ -38,34 +37,24 @@ Game.create(
   photo: 'nkids.jpg',
   user: user1
 )
-2.times do
-  Game.create(
-    name: "Jack the Ripper",
-    city: "London",
-    start: "12-06-2022 10:00",
-    end: "12-06-2022 13:00",
-    photo: 'placeholder.png',
-    user: user1
-  )
 
-  Game.create(
-    name: "Jurassic Park",
-    city: "London",
-    start: "01-06-2022 10:00",
-    end: "01-06-2022 13:00",
-    photo: 'placeholder.png',
-    user: user1
-  )
+Game.create(
+  name: "Jack the Ripper",
+  city: "London",
+  start: "12-06-2022 10:00",
+  end: "12-06-2022 13:00",
+  photo: 'placeholder.png',
+  user: user1
+)
 
-  Game.create(
-    name: "Ancient Aliens",
-    city: "London",
-    start: "14-06-2022 10:00",
-    end: "14-06-2022 13:00",
-    photo: 'placeholder.png',
-    user: user1
-  )
-end
+Game.create(
+  name: "Ancient Aliens",
+  city: "London",
+  start: "14-06-2022 10:00",
+  end: "14-06-2022 13:00",
+  photo: 'placeholder.png',
+  user: user1
+)
 
 puts "Created #{Game.count} games!"
 
@@ -109,11 +98,11 @@ puts "Created #{Game.count} games!"
 
 # puts "Created 2 teams per game!"
 
-puts "Creating 6 challenges..."
+puts "Creating challenges..."
 
 # 16.times do
   Challenge.create(
-    question: "How many lions are there in trafalgar square?",
+    question: "How many lions are there in Trafalgar Square?",
     answer_1: "3",
     answer_2: "1",
     answer_3: "8",
@@ -126,8 +115,8 @@ puts "Creating 6 challenges..."
   Challenge.create(
     question: "How many capsules does the London Eye have?",
     answer_1: "18",
-    answer_2: "42",
-    answer_3: "69",
+    answer_2: "99",
+    answer_3: "48",
     answer_correct: "32",
     points: 10,
     location: "London Eye",
@@ -135,38 +124,18 @@ puts "Creating 6 challenges..."
   )
 
   Challenge.create(
-    question: "What is the common name for The Royal Courts of Justice?",
-    answer_1: "Elizabeth's Naughty List",
-    answer_2: "The Loyal Courts of Justice",
-    answer_3: "The Loyal Courts of Justyce",
-    answer_correct: "The Law Courts",
+    question: "What colour are the dials and clock hands of Big Ben?",
+    answer_1: "Le Wagon red",
+    answer_2: "Black",
+    answer_3: "Neon green",
+    answer_correct: "Prussian blue",
     points: 10,
-    location: "The Royal Courts of Justice",
-    game: Game.first
-  )
-  Challenge.create(
-    question: "How many rooms in the Buckingham Palace?",
-    answer_1: "Between 300 and 500",
-    answer_2: "Over 1000",
-    answer_3: "Less than 700",
-    answer_correct: "Over 700",
-    points: 10,
-    location: "Buckingham Palace",
-    game: Game.first
-  )
-  Challenge.create(
-    question: "Which river does the Palace of Westminster sit on?",
-    answer_1: "River Island",
-    answer_2: "Amazon River",
-    answer_3: "Lake Toba",
-    answer_correct: "River Thames",
-    points: 10,
-    location: "Palace of Westminster",
+    location: "House of Commons, Westminster, London SW1A 0AA, United Kingdom",
     game: Game.first
   )
 # end
 
-puts "Created 6 challenges!"
+puts "Created #{Challenge.count} challenges!"
 
 # 16.times do
 #   Completion.create(
